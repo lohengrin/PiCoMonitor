671a1549e38b678c4bf17e48f4feaec5f70d424c
lohengrin <guillaume@famille-millet.fr>
2023-02-20 01:25:10 +0100

    Windows port of python side

--- PiCoMonitor.py
+++ PiCoMonitor.py
@@ -4,6 +4,7 @@
 import psutil, json
 import time
 import serial
+import sys
 
 def disk_usage(json_key):
     hdd = psutil.disk_partitions()
@@ -12,14 +13,22 @@ def disk_usage(json_key):
         path = each.mountpoint
         fstype = each.fstype
         # filter devices
-        if fstype != "ext4":
-            continue
-        if path.startswith("/var/"):
-            continue
+        if sys.platform.startswith("linux"):
+            # filter devices
+            if fstype != "ext4":
+                continue
+            if path.startswith("/var/"):
+                continue
 
-        label = path
-        if path.startswith("/mnt/"):
-            label = path.replace("/mnt/", "")
+            label = path
+            if path.startswith("/mnt/"):
+                label = path.replace("/mnt/", "")
+        else:
+            if fstype != "NTFS":
+                continue
+            if fstype == "":
+                continue
+            label = path
 
         drive = psutil.disk_usage(path)
         total = drive.total
@@ -40,8 +49,11 @@ def cpu_usage(json_key):
     json_key['CPU'] = cpu_usage
 
 def cpu_temp(json_key):
-    temps = psutil.sensors_temperatures()
-    json_key['TEMP'] = temps['amdgpu'][0][1]
+    if sys.platform.startswith("linux"):
+        temps = psutil.sensors_temperatures()
+        json_key['TEMP'] = temps['amdgpu'][0][1]
+    else:
+        json_key['TEMP'] = 0.0
 
 def percent_mem(json_key):
     mem_percent = psutil.virtual_memory().percent
@@ -50,7 +62,10 @@ def percent_mem(json_key):
 
 while True:
     try:
-        ser = serial.Serial('/dev/ttyACM0')  # open serial port
+        if sys.platform.startswith("linux"):
+            ser = serial.Serial('/dev/ttyACM0')  # open serial port
+        else:
+            ser = serial.Serial('COM3')  # open serial port
         print('Opening:' + ser.name)
 
         while True:
@@ -59,8 +74,7 @@ while True:
             cpu_temp(json_key)
             percent_mem(json_key)
             disk_usage(json_key)
-            print(json.dumps(json_key), end='\r')
-
+            #print(json.dumps(json_key), end='\r')
             data = json.dumps(json_key) 
             ser.write(data.encode('ascii'))
 
